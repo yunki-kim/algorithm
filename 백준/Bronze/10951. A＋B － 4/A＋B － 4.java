7aeb7f6fecb446583f055dd09910401d3298b445
Yuki <63786040+yunki-kim@users.noreply.github.com>
2022-12-05 14:05:30 +0900

    [Bronze V] Title: A+B - 4, Time: 124 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String input;
+        while ((input = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(input, " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            sb.append(A + B).append("\n");
+        }
+        br.close();
+        System.out.print(sb);
+    }
+}
