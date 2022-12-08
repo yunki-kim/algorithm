2e37039f3aa3181c4e8343c37f50917e7e11c617
Yuki <63786040+yunki-kim@users.noreply.github.com>
2022-12-08 15:15:25 +0900

    [Bronze II] Title: 나머지, Time: 124 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean[] arr = new boolean[42];
+        for (int i = 0; i < 10; i++) {
+            arr[Integer.parseInt(br.readLine()) % 42] = true;
+        }
+        br.close();
+
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
