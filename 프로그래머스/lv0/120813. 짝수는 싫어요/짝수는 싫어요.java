8d1178791fa42ecac779673cdece7bd9edc4001b
Yuki <63786040+yunki-kim@users.noreply.github.com>
2022-12-21 14:09:06 +0900

    [level 0] Title: 짝수는 싫어요, Time: 2.65 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) list.add(i);
+        }
+
+        return list.stream()
+                .mapToInt(i -> i)
+                .toArray();
+    }
+}
