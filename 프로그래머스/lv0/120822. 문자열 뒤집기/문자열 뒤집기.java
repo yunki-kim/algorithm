d568637b791d4c20ca5507e5531f319e122c340b
Yuki <63786040+yunki-kim@users.noreply.github.com>
2022-12-22 20:01:00 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.02 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,6 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        return sb.append(my_string).reverse().toString();
+    }
+}
