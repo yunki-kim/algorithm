94bbce07fba5af673e74b6d344d547c13b3a7985
Yuki <63786040+yunki-kim@users.noreply.github.com>
2022-12-28 14:29:53 +0900

    [level 0] Title: 소인수분해, Time: 2.45 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+class Solution {
+    public int[] solution(int n) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 2; i <= n; i++) {
+            while (n % i == 0) {
+                set.add(i);
+                n /= i;
+            }
+        }
+        if (n != 1) set.add(n);
+
+        return set.stream().mapToInt(Integer::intValue).sorted().toArray();
+    }
+}
