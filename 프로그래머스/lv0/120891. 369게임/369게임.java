a0d3fbf041b76d6a3fb577cdbbb31f377181eac6
Yuki <63786040+yunki-kim@users.noreply.github.com>
2022-12-30 17:33:20 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.java
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -1,11 +1,9 @@
 class Solution {
     public int solution(int order) {
-        String orderStr = "" + order;
         int count = 0;
-        for (int i = 0; i < orderStr.length(); i++) {
-            if (orderStr.charAt(i) == '3'
-                    || orderStr.charAt(i) == '6'
-                    || orderStr.charAt(i) == '9') count++;
+        while (order != 0) {
+            if (order % 10 == 3 || order % 10 == 6 || order % 10 == 9) count++;
+            order /= 10;
         }
         return count;
     }
