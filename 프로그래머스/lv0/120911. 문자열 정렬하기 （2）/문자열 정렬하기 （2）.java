8eabb97bbbe5a1e54de29e505d2fdb3fe0407f1a
Yuki <63786040+yunki-kim@users.noreply.github.com>
2023-01-05 13:51:12 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 3.13 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string) {
+        return Arrays.stream(my_string.toLowerCase().split(""))
+                .sorted()
+                .collect(Collectors.joining());
+    }
+}
