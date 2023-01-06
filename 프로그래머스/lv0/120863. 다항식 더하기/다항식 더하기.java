293bdf4091317ee977ab9a08fdcf408348850559
Yuki <63786040+yunki-kim@users.noreply.github.com>
2023-01-06 17:18:55 +0900

    [level 0] Title: 다항식 더하기, Time: 0.14 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
@@ -1,15 +1,11 @@
 class Solution {
     public String solution(String polynomial) {
-        String[] stringArray = polynomial.split(" ");
         int sumX = 0;
         int sum = 0;
-        for (String string : stringArray) {
+        for (String string : polynomial.split(" ")) {
             if (string.equals("+")) continue;
             if (string.contains("x")) {
-                String temp = string.replace("x", "");
-                if (temp.equals("")) sumX++;
-                else sumX += Integer.parseInt(temp);
-
+                sumX += string.equals("x") ? 1 : Integer.parseInt(string.replaceAll("x", ""));
             } else sum += Integer.parseInt(string);
         }
 
@@ -19,7 +15,6 @@ class Solution {
                     : sb.append(sumX).append("x").append(" + ").append(sum).toString();
         }
         if (sumX != 0) return sumX == 1 ? sb.append("x").toString() : sb.append(sumX).append("x").toString();
-
         return sum != 0 ? sb.append(sum).toString() : "0";
     }
 }
