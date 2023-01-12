da27a0751401b9c94a242e3b04de459ae46d8712
Yuki <63786040+yunki-kim@users.noreply.github.com>
2023-01-12 21:12:43 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int[] common) {
+        if (common[1] - common[0] == common[2] - common[1]) {
+            return common[common.length - 1] + (common[1] - common[0]);
+        }
+        else return common[common.length -1] * (common[1] / common[0]);
+    }
+}
