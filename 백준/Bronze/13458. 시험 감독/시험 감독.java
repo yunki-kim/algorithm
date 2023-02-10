e3905852e2fa04180e748942bce8b47bac5c7151
Yuki <63786040+yunki-kim@users.noreply.github.com>
2023-02-10 21:10:12 +0900

    [Bronze II] Title: 시험 감독, Time: 472 ms, Memory: 92224 KB -BaekjoonHub

---
+++ 백준/Bronze/13458. 시험 감독/시험 감독.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < A.length; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        br.close();
+
+        long count = N;
+        for (int takers : A) {
+            takers -= B;
+            if (takers <= 0) continue;
+
+            count += takers / C;
+            if (takers % C != 0) count++;
+        }
+        System.out.println(count);
+    }
+}
