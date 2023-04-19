51d3d34f5331d96a3ee45e639f7d1f7ddca8209e
Yuki <63786040+yunki-kim@users.noreply.github.com>
2023-04-19 21:26:44 +0900

    [Bronze V] Title: 꼬마 정민, Time: 132 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long sum = 0;
+        for (int i = 0; i < 3; i++) {
+            sum += Long.parseLong(st.nextToken());
+        }
+        System.out.println(sum);
+    }
+}
